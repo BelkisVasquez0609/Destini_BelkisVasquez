0a63af21aab0dcc24764054d1a91ade799e69de5
BELKIS YAZMIN VASQUEZ PENA <belkisvasquez@outlook.es>
2023-06-06 19:44:07 -0400

    First commit

---
+++ android/app/src/main/kotlin/com/example/destini_belkis_vasquez/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.destini_belkis_vasquez
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:destini_belkis_vasquez/story_brain.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(destini_belkis_vasquez());
+
+class destini_belkis_vasquez extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
+StoryBrain storyBrain = StoryBrain();
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        //TODO: Step 1 - Add background.png to this Container as a background image.
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: const BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+               Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
+                    storyBrain.getStory(),
+                    style: const TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    //storyBrain.nextStory(1);
+                    //TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
+                  },
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.red)
+                  ),
+                  child:  Text(
+                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
+                    storyBrain.getChoice1(),
+                    style: const TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                 child: Visibility(
+                   //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                    visible: storyBrain.buttonShouldBeVisible(),
+                 child: TextButton( //FlatButton is deprecated and totally removed from flutter after flutter 2.1.0 Use TextButton
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      storyBrain.nextStory(2);
+                    },
+                    style: const ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(Colors.blue)
+                    ),
+
+                    child: Text(
+                      //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
+                      storyBrain.getChoice2(),
+                      style: const TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
